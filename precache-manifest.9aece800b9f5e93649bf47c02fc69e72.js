254c4e8ef82031d3deed971cb813bbe36d9d38c3
Jenkins <jenkins@common-infra-jenkins-1.asia-south1-c.c.crio-vpn-common.internal>
2020-09-07 10:12:45 +0530

    Updates

---
+++ precache-manifest.9aece800b9f5e93649bf47c02fc69e72.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c14280f343f59726dd15fcb696dc724",
+    "url": "/Crio-PhonePe-Aug-2020-ankit-chaudhary-phonepe/index.html"
+  },
+  {
+    "revision": "f3ce87d19f0d87d8046f",
+    "url": "/Crio-PhonePe-Aug-2020-ankit-chaudhary-phonepe/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "e2256be386d4f357e3fa",
+    "url": "/Crio-PhonePe-Aug-2020-ankit-chaudhary-phonepe/static/css/main.5db1380d.chunk.css"
+  },
+  {
+    "revision": "f3ce87d19f0d87d8046f",
+    "url": "/Crio-PhonePe-Aug-2020-ankit-chaudhary-phonepe/static/js/2.75e12de7.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/Crio-PhonePe-Aug-2020-ankit-chaudhary-phonepe/static/js/2.75e12de7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2256be386d4f357e3fa",
+    "url": "/Crio-PhonePe-Aug-2020-ankit-chaudhary-phonepe/static/js/main.1d61aa0e.chunk.js"
+  },
+  {
+    "revision": "63cf46b48e31c73790d5",
+    "url": "/Crio-PhonePe-Aug-2020-ankit-chaudhary-phonepe/static/js/runtime-main.a185f688.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/Crio-PhonePe-Aug-2020-ankit-chaudhary-phonepe/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/Crio-PhonePe-Aug-2020-ankit-chaudhary-phonepe/static/media/crio-logo.63be71d0.svg"
+  }
+]);
